e1e80163fbf16048fd4f5267f635fec817a4c67b
unknown <farhanishraqtushar@gmail.com>
2024-09-01 22:02:29 +0600

    quiz part added

---
+++ Module/0000_Javascript/0019_js_loop/10_quiz.js
@@ -0,0 +1,23 @@
+
+// const array = [1, 2, 3, 4, 5, 6, 7];
+
+// for (let i = 1; i < array.length; i++) {
+//  if (i == 5) {
+//  continue;
+//  }
+//  console.log(array[i]);
+// }
+
+// for(let i=5; i<=100; i++){
+//     console.log(i);
+// }
+
+// for (let i = 0; i < 5; i+=3) {
+//     console.log(i);
+//    }
+
+let i = 0;
+while (i < 5) {
+ console.log(i);
+ i++;
+}
