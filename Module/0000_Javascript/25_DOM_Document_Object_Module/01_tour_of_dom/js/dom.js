af818e93019d80b5452206925f0687261ec9cd2c
unknown <farhanishraqtushar@gmail.com>
2024-09-15 20:00:07 +0600

    DOM

---
+++ Module/0000_Javascript/25_DOM_Document_Object_Module/01_tour_of_dom/js/dom.js
@@ -0,0 +1,8 @@
+const sections = document.querySelectorAll('section');
+for(const section of sections){
+    section.style.border = '2px solid red'; 
+    section.style.marginBottom = '5px';
+    section.style.borderRadius = '15px';
+    section.style.paddingLeft = '7px';
+    section.style.backgroundColor = 'gray'
+}
