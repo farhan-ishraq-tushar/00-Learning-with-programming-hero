1e89e3716dd9048545f469f6e91c411abe87085d
unknown <farhanishraqtushar@gmail.com>
2024-09-27 12:21:40 +0600

    js what we learned list

---
+++ 00_Module/0000_Javascript/0029_5_JS_Extra/recap.js
@@ -0,0 +1,33 @@
+// ".querySelectorAll('')"  selects elements inside element 
+document.querySelectorAll('');
+
+// Search javaScript WAT
+// search javaScript undefined check
+
+// What we have learned
+/**
+ 1. how to use html and css with js
+
+ 2. get element or elements form dom
+    2-1: getElementById
+    2-2: getElementsByClassName
+    2-3: querySelector
+    2-4: querySelectorAll
+
+ 3. dynamically add element to the dom: document.createElement
+    3-5: set value to the innerHTML
+
+ 4. get and set value from/to a DOM element
+    4-1: use innerText (h1, p, div, span, strong)
+    4-2: use value (input, textArea)
+
+ 5. add event lister (event handler)
+    5-1: click, focus, blur, onchange, keyup
+    5-2: addEventListener
+    5-3: onClick = "myHandler()"
+
+ 6. use function to reduce duplicate code 
+
+ 7. event delegate and event bubble
+  
+ */
